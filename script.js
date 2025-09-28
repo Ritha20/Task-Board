0fe88989ae6ad3d5332f6ad53cd4cf48dd5678f1
Irakoze Ritha Theonestine <irakozeritha20@gmail.comclear>
2025-09-28 17:51:40 +0200

    adding task using array

---
+++ script.js
@@ -0,0 +1,8 @@
+let tasks = [
+    {id:1, name: "Completing Task Board Assignment", dueDate: "2025-09-28",status: "Pendding"},
+    {id:2, name:"studying objects",dueDate: "2025-10-05",status: "pending",},
+    {id:3, name:"shopping", dueDate:"2025-10-20", status:"pending"},
+    {id:4, name:"writing", dueDate:"2025-10-01", status:"completed"},
+    {id:5, name:"preparing", dueDate:"2025-11-04", status:"pending"}
+];
+let nextId = 6;
